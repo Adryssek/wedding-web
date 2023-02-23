c35867516440a9cc666f706d9efd96920d687efa
Adri Chochelova <adriana@chochelova.eu>
2023-02-23 21:31:56 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var map = L.map('map').setView([50.1025, 14.407190], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var greenIcon = L.icon({
+    iconUrl: 'Pictures/heart-icon.svg',
+
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([50.1025, 14.407190], {icon: greenIcon}).addTo(map);
