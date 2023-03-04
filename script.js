ccb9a986e2d1e2be2df030ea121d1d169e1145b3
Adri Chochelova <adriana@chochelova.eu>
2023-03-04 09:28:56 +0100

    small changes to style

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 var greenIcon = L.icon({
     iconUrl: 'Pictures/heart-icon.svg',
-
-    iconSize:     [38, 95], // size of the icon
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize:     [46, 46], // size of the icon
+    iconAnchor:   [23, 46], // point of the icon which will correspond to marker's location
+  //  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 L.marker([50.1025, 14.407190], {icon: greenIcon}).addTo(map);
