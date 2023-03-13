e92737747ee7363cb662fb440c8e48192dd72a13
Adri Chochelova <adriana@chochelova.eu>
2023-03-13 20:12:08 +0100

    change icons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var greenIcon = L.icon({
-    iconUrl: 'Pictures/heart-icon.svg',
+    iconUrl: 'Pictures/wedding-location-svgrepo-com.svg',
     iconSize:     [46, 46], // size of the icon
     iconAnchor:   [23, 46], // point of the icon which will correspond to marker's location
   //  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
